2143b0192b2b2de3e75cf510db9c3e0240d0cbe6
Livia <109006719+lialenc14@users.noreply.github.com>
2023-08-28 09:29:08 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -36,3 +36,11 @@ const createTrWithGivenValues = (nome, faltas) => {
 
     return tr;
 }
+function setObjectLocalStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+
+function getObjectLocalStorage(key){
+    var value = localStorage.getItem(key);
+    return value && JSON.parse(value);
+}
